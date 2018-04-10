6b1e4fcf3936d825f2e08edf037ad78c723d019b
root <root@ip-172-31-30-54.us-west-2.compute.internal>
2018-04-10 10:50:28 +0000

    fdg

---
+++ src/main/java/mahi/in/com/App.java
@@ -0,0 +1,13 @@
+package mahi.in.com;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
